7b75e901cbc14a73ad7f1d59dbc60b1e39a7e641
Anton <antonburchak01@gmail.com>
2020-04-26 20:37:39 +0300

    HW6 Completed

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,17 +1,44 @@
+const word = prompt('Type any word'),
+    evenNumber = 2;
 
-const word = prompt('Type any word'), evenNumber = 2;
+const styledOut = (word, subword, type = 'even') => {
+
+    const char = 
+        type === 'even' ? 
+        word.slice(subword[0], subword[1]) : 
+        word[subword];
+
+    const firstPart = 
+        type === 'even' ?
+        word.slice(0, word.indexOf(char)) : 
+        word.slice(0, subword);
+
+    const lastPart =
+        type === 'even' ?
+        word.slice(word.indexOf(char) + char.length, word.length) :
+        word.slice(subword + char.length, word.length);
+
+    const str = `${firstPart}**${char.toUpperCase()}**${lastPart}`;
+
+    return str;
+
+}
 
 if (word.trim().length !== 0) {
+
     const even = (word) => {
         const middle = [(word.length - 1) / evenNumber, (word.length - 1) / evenNumber + evenNumber];
-        return alert(word.slice(middle[0], middle[1]));
+        return alert(
+            `${styledOut(word, middle, 'even')}\n Middle characters of the word: '${word.slice(middle[0], middle[1])}'
+            `);
     };
+
     const odd = (word) => {
         const middle = (word.length - 1) / evenNumber;
-        return alert(word[middle]);
+        return alert(`${styledOut(word, middle, 'odd')}\nMiddle character of the word: '${word[middle]}'`);
     };
-    
+
     word.length % evenNumber === 0 ? even(word) : odd(word);
 } else {
     alert('Invalid value');
-}
+}   
